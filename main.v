5a630a413cead83a3f4ff6d58814726afb19f4dc
CM-IV <cmathIV@protonmail.com>
2021-12-15 15:43:25 -0600

    First commit of fizz buzz in v-lang

---
+++ main.v
@@ -0,0 +1,35 @@
+module main
+
+import os { input }
+
+fn fizz_buzz() {
+
+	for i := 1; i <=100; i++ {
+
+		if i % 3 == 0 && i % 5 == 0 {
+
+			println('FizzBuzz')
+
+		} else if i % 3 == 0 {
+
+			println('Fizz')
+
+		} else if i % 5 == 0 {
+
+			println('Buzz')
+
+		} else {
+
+			println(i)
+
+		}
+
+	}
+
+}
+
+fn main() {
+
+	fizz_buzz()
+
+}
